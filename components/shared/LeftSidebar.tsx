c4d85ac3ccbff7c8ba6d8c448485ccc41cc396a4
Jyoti Suman <jyotisumanswexpprac@gmail.com>
2023-08-06 23:56:28 +0530

    Profile page ✅

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 import { sidebarLinks } from "@/constants"
 import Link from "next/link";
 import Image from "next/image";
@@ -9,12 +9,15 @@ import { usePathname, useRouter }
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {sidebarLinks.map((link) => {
                     const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                    if(link.route === '/profile') link.route = `${link.route}/${userId}`
 
                     return (
                         <Link
