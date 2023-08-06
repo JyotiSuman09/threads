812744a4b67e0660eace88e23a38da96093c7739
Jyoti Suman <jyotisumanswexpprac@gmail.com>
2023-08-06 12:23:03 +0530

    mongodb connected

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -3,7 +3,6 @@
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -20,6 +19,8 @@ import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -37,6 +38,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(userValidation),
@@ -56,12 +59,25 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         if (hasImageChanged) {
             const imgRes = await startUpload(files);
 
-            if(imgRes && imgRes[0].fileUrl) {
+            if (imgRes && imgRes[0].fileUrl) {
                 values.profile_photo = imgRes[0].fileUrl;
             }
         }
 
-        // TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if (pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/')
+        }
     }
 
     const handleImage = (
